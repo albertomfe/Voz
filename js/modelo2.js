d4e9b82b2efa900d741de90b494b21c09ba311a6
albertomfe <albertomfe@gmail.com>
2019-05-16 18:00:53 -0500

    Se agrego la identificacion de Edades

--- js/modelo2.js
+++ js/modelo2.js
@@ -1,10 +1,14 @@
       //variables globales
+      var fecha_viaje="";
       var num_adultos="";
       var num_menores="";
+      var edades=[];
+      var num_noches="";
       var cadenaInvertida="";
       var espacios=0;
       var tam_cadena=0;
       var texto_dictado="";
+
 
     $( document ).ready(function()
     {
@@ -35,6 +39,14 @@
             num_adultos=identificarOcupacion("adultos","adulto",texto_dictado);
             //-----------------------------------------------MENORES-------------------------------------------------------------------------------
             num_menores=identificarOcupacion("menores","menor",texto_dictado);
+            //-----------------------------------------------Edades-------------------------------------------------------------------------------
+            edades=identificarEdades(texto_dictado);
+            //-----------------------------------------------Noches-------------------------------------------------------------------------------
+            num_noches=identificarOcupacion("noches","noche",texto_dictado);
+
+            //console.log(edades);
+            //-----------------------------------------------FECHA DE VIAJE-------------------------------------------------------------------------------
+            //fecha_viaje=identificarFecha(texto_dictado);
 
 
             $('#capture').text("Hablar");
@@ -43,6 +55,14 @@
             console.log('Termine de Escuchar .......');
             console.log('adultos='+num_adultos);
             console.log('menores='+num_menores);
+
+            let k=0;
+            while(k<edades.length)
+            {
+              console.log('menor '+(k+1)+'='+edades[k]+' Años');
+              k++;
+            }
+            console.log('noches='+num_noches);
         }
 
         //Precionar el Boton
@@ -54,7 +74,6 @@
           speech.stopCapture();
         }
         });
-
       });
 
 
@@ -100,6 +119,103 @@
         }
       }
 
+
+      /*
+      *Metodo para Identificar la Fecha de Viaje
+      *recibe como parametro el texto dictado
+      *
+      */
+      function identificarFecha(texto_dictado)
+      {
+          //si no se dicto nada no ejecutar codigo
+          if(texto_dictado==""){ return ""; }
+
+          if(texto_dictado.indexOf("enero")!=-1)  {  comienzaEn=texto_dictado.indexOf("enero");  }
+          else if(texto_dictado.indexOf("febrero")!=-1){  comienzaEn=texto_dictado.indexOf("febrero");}
+          else if(texto_dictado.indexOf("marzo")!=-1){  comienzaEn=texto_dictado.indexOf("marzo");}
+          else if(texto_dictado.indexOf("abril")!=-1){  comienzaEn=texto_dictado.indexOf("abril");}
+          else if(texto_dictado.indexOf("mayo")!=-1){  comienzaEn=texto_dictado.indexOf("mayo");}
+          else if(texto_dictado.indexOf("junio")!=-1){  comienzaEn=texto_dictado.indexOf("junio");}
+          else if(texto_dictado.indexOf("julio")!=-1){  comienzaEn=texto_dictado.indexOf("julio");}
+          else if(texto_dictado.indexOf("agosto")!=-1){  comienzaEn=texto_dictado.indexOf("agosto");}
+          else if(texto_dictado.indexOf("septiembre")!=-1){  comienzaEn=texto_dictado.indexOf("septiembre");}
+          else if(texto_dictado.indexOf("octubre")!=-1){  comienzaEn=texto_dictado.indexOf("octubre");}
+          else if(texto_dictado.indexOf("noviembre")!=-1){  comienzaEn=texto_dictado.indexOf("noviembre");}
+          else if(texto_dictado.indexOf("diciembre")!=-1){  comienzaEn=texto_dictado.indexOf("diciembre");}
+
+          console.log('fecha comienza en  '+comienzaEn);
+
+      }
+
+
+
+      /*
+      *Metodo para Identificar las Edades de Los menores
+      *recibe como parametro el texto dictado
+      */
+      function identificarEdades(texto_dictado)
+      {
+        //si no se dicto nada no ejecutar codigo
+        edades=[];
+        if(texto_dictado==""){ return edades; }
+
+        //buscar coincidencia de Edades
+        if(texto_dictado.indexOf("años")!=-1){
+            comienzaEn=texto_dictado.indexOf("años");
+        }
+        else if(texto_dictado.indexOf("año")!=-1){
+            comienzaEn=texto_dictado.indexOf("año");
+        }
+        else{  comienzaEn=-1;  }
+
+
+        //si encuentra la cadena
+        if(comienzaEn!=-1)
+        {
+          //empezar a recorrer apartir de la posicion que empieza la palabra clave en sentido inverso
+           espacios=0;
+           var pos=0;//manejar pociciones de las edades
+           edades[0]="";
+           for(var i=(comienzaEn-1);i>=0;i--)
+           {
+             if(texto_dictado[i]=="y"){ pos++; edades[pos]="";/*se crea la edad del menor*/  }
+             if(texto_dictado[i]==" "){ espacios++;  }
+             else
+             { //si no hay espacios alamcenar
+               //console.log('entre pos='+pos);
+                if(texto_dictado[i]!="y" && !isNaN(texto_dictado[i]) )
+                {
+                  edades[pos]+=texto_dictado[i];
+                  console.log('pos= '+pos+' edades[pos] ='+edades[pos]+' text '+texto_dictado[i]);
+                }
+              }
+             if(texto_dictado[i]!="y" && isNaN(texto_dictado[i]) ){break;}//cuando encuentre dos espacios que representan el corte de cadena deja de concatenar a el numero de adultos
+           }
+
+           let tam_edades=edades.length;
+           let invertida=[];
+           for(var j=0;j<tam_edades;j++)
+           {
+             console.log(edades[j]+'tam='+edades[j].length);
+             //invertir la cadena
+
+             let x=(edades[j].length)-1;
+             invertida[j]="";
+             while (x>=0) {
+               invertida[j]+=edades[j][x];
+               x--;
+             }
+           }
+           return invertida;
+        }
+        else
+        {
+          return edades;
+        }
+
+
+
+      }
 
 
       /*
@@ -176,6 +292,9 @@
       {
           switch(texto)
           {
+            case "una":
+              return 1;
+            break;
             case "un":
               return 1;
             break;
